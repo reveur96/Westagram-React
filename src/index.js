299769e9418f0802a279e4fa5f20f4bfb7450893
dlfrlwkd11@daum.net <dlfrlwkd11@daum.net>
2023-04-18 20:38:11 +0900

    Add : 1-1. Router 컴포넌트 구현

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Login from './pages/Login/Login'
-import Main from './pages/Main/Main'
-import './pages/Login/Login.css'
-import './pages/Main/Main.css'
+import Router from './Router'
 import './styles/common.css'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Main />);
+root.render(<Router />);
 
