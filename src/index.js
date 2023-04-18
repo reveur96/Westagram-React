f24c8354b8ba09a12b1a89350a8a69cc9c600ea5
dlfrlwkd11@daum.net <dlfrlwkd11@daum.net>
2023-04-18 22:09:28 +0900

    Modify :assignment2 Sass

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Router from './Router'
-import './styles/common.css'
+import './styles/common.scss'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
